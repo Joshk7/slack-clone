4a46883ef05ade2c53f60cf9dbecb688358a698e
JoshK7 <joshk7683@gmail.com>
2024-10-01 16:37:09 -0500

    toolbar component

---
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Toolbar } from "./toolbar";
+
+type WorkspaceIdLayoutProps = {
+  children: React.ReactNode;
+};
+
+const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+  return (
+    <div className="h-full">
+        <Toolbar />
+        {children}
+    </div>
+    );
+};
+
+export default WorkspaceIdLayout;
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,15 +1,12 @@
-type WorkSpaceIdPageProps = {
-  params: {
-    workspaceId: string;
-  };
-};
+"use client";
 
-const WorkspaceIdPage = ({ params }: WorkSpaceIdPageProps) => {
-  return (
-    <div>
-        ID: {params.workspaceId}
-    </div>
-)
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+const WorkspaceIdPage = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+  return <div>Data: {JSON.stringify(data)}</div>;
 };
 
 export default WorkspaceIdPage;
