eb314fb9e907f8e26c0e2766a32b1747e2c919a0
JoshK7 <joshk7683@gmail.com>
2024-10-06 19:29:48 -0500

    workspace header with channels

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 type WorkspaceIdLayoutProps = {
   children: React.ReactNode;
@@ -10,13 +17,26 @@ type WorkspaceIdLayoutProps = {
 const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
   return (
     <div className="h-full">
-        <Toolbar />
-        <div className="flex h-[calc(100vh-40px)]">
-            <Sidebar />
-            {children}
-        </div>
+      <Toolbar />
+      <div className="flex h-[calc(100vh-40px)]">
+        <Sidebar />
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel 
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+            autoSave="cj-workspace-layout"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel
+            minSize={20}
+          >{children}</ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </div>
-    );
+  );
 };
 
 export default WorkspaceIdLayout;
