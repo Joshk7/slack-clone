1de3f673cbfd5ab77af5d3f98c9b4af96b2ff4aa
JoshK7 <joshk7683@gmail.com>
2024-10-08 21:17:57 -0500

    workspace preferences adding and removing workspaces

---
+++ src/components/jotai-provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Provider } from "jotai";
+
+type JotaiProviderProps = {
+    children: React.ReactNode;
+}
+
+export const JotaiProvider = ({ children }: JotaiProviderProps) => {
+    return (
+        <Provider>
+            {children}
+        </Provider>
+    );
+}
